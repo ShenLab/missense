e301ba5c4b4289f94a0362c08f4e185f686f0f54
Hongjian Qi <hq2130@c2b2ysld4.arcs.cpmc.columbia.edu>
2017-05-25 11:54:53 -0400

    update on alienware

--- code/train_models.py
+++ code/train_models.py
@@ -4,7 +4,7 @@ from models import CNN_Model, CNN_Model_Mode6
 
 weights_path = None #'../models/cnn_model.hdf5'
 model = CNN_Model_Mode6(weights_path=weights_path, train_flag=True, verbose=1,
-                       nb_epoch=100, batch_size=64, 
+                       nb_epoch=10, batch_size=64, 
                        fname='../data/input_data.csv', f_out = '../data/output/output_data_mode5.csv')
 
 model.train(sub_sample=True)
