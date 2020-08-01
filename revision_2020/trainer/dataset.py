38983d40fc03ae97e63bfe7f21e69dd641479cba
zhanghaicang <zhanghaicang@gmail.com>
2020-08-01 18:14:49 -0400

    add:trainer dataset

---
+++ revision_2020/trainer/dataset.py
@@ -0,0 +1,44 @@
+from sklearn.model_selection import train_test_split
+import numpy as np
+import pandas as pd
+
+HS_fea_names = [
+    "FATHMM_converted_rankscore", "Eigen-phred",
+    "phastCons20way_mammalian_rankscore", "blosum62", "gc_content", "ASA",
+    "lofz", "prec", "pli", "secondary_E", "secondary_H", "secondary_C",
+    "complex_CORUM", "preppi_counts", "interface", "ubiquitination", "BioPlex",
+    "pam250", "SUMO_score", "phospho_score", "domino"
+]
+
+HIS_fea_names = [
+    "MutationAssessor_score_rankscore", "VEST3_rankscore",
+    "Polyphen2_HDIV_rankscore", "Polyphen2_HVAR_rankscore",
+    "SIFT_converted_rankscore", "PROVEAN_converted_rankscore",
+    "FATHMM_converted_rankscore", "LRT_converted_rankscore",
+    "Eigen-PC-raw_rankscore", "Eigen-phred", "Eigen-PC-phred",
+    "phyloP20way_mammalian_rankscore", "GERP++_RS_rankscore",
+    "SiPhy_29way_logOdds_rankscore", "phastCons100way_vertebrate_rankscore",
+    "fathmm-MKL_coding_rankscore", "phyloP100way_vertebrate_rankscore",
+    "MutationTaster_converted_rankscore", "phastCons20way_mammalian_rankscore",
+    "blosum62", "pam250", "SUMO_score", "phospho_score", "lofz", "prec", "pli",
+    "s_het_log", "secondary_E", "secondary_H", "complex_CORUM",
+    "preppi_counts", "ASA", "secondary_C", "gc_content", "interface",
+    "ubiquitination", "BioPlex", "obs_exp"
+]
+
+
+def get_train_valiate(input_config, gene_type):
+    path = input_config['train'][gene_type]
+    df = pd.read_csv(path)
+    if gene_type == 'HIS':
+        names = HIS_fea_names
+    else:
+        names = HS_fea_names
+    x = df[names].values
+    y = df['target'].values
+
+    indices = np.arange(x.shape[0])
+    X_train, X_test, y_train, y_test, idx_train, idx_test = train_test_split(
+        x, y, indices, test_size=0.2, random_state=42)
+
+    return (X_train, y_train), (X_test, y_test)
---
+++ revision_2020/trainer/trainer.py
@@ -0,0 +1,102 @@
+import random
+import time
+import glob
+import logging
+import os
+import numpy as np
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras.layers import Input, Conv1D, Activation, Flatten, Dense
+import dataset
+import argparse
+import json
+
+class Trainer(object):
+    def __init__(self, config):
+        self.train_config = config['train']
+        self.model_config = config['model']
+        self.input_config = config['input']
+
+        self.gene_type = self.model_config['gene_type']
+        if self.gene_type == 'HIS':
+            self.feature_dim = len(dataset.HIS_fea_names)
+        else:
+            self.feature_dim = len(dataset.HS_fea_names)
+
+    def _build_model(self):
+
+        activation = 'relu'
+        kernel_size = self.model_config.get('kernel_size', 3)
+        filters = self.model_config.get('filters', 32)
+        block_num = self.model_config.get('block_num', )
+
+        input_ = Input(shape=(self.feature_dim, 1))
+        x = Conv1D(filters, kernel_size, padding='same',
+                   activation=activation)(input_)
+        for _ in range(block_num):
+            y = Conv1D(filters,
+                       kernel_size,
+                       padding='same',
+                       activation=activation)(x)
+            y = Conv1D(filters, kernel_size, padding='same')(y)
+            x = x + y
+            x = Activation(activation)(x)
+
+        x = Flatten()(x)
+        x = Dense(512, activation=activation)(x)
+        x = Dense(1, name='dense1')(x)
+        act2 = Activation('sigmoid', name='act2')(x)
+
+        model = keras.Model(input_, act2)
+
+        return model
+
+    def train(self):
+        model = self._build_model()
+        #model.summary()
+        model.compile(loss='binary_crossentropy',
+                      optimizer='adam',
+                      metrics=['accuracy'])
+
+        early_stop_callback = tf.keras.callbacks.EarlyStopping(
+            monitor='val_loss', patience=5, mode='auto', restore_best_weights=True)
+
+
+        train_data, val_data = dataset.get_train_valiate(
+            self.input_config, self.gene_type)
+
+        batch_size = self.train_config.get('batch_size', 64)
+
+        model.fit(x=train_data[0],
+                  y=train_data[1],
+                  batch_size=32,
+                  epochs=256,
+                  validation_data=val_data,
+                  callbacks=[early_stop_callback])
+
+        model_path = f'{self.train_config["base_dir"]}/best_model.h5'
+        model.save(model_path)
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--config', type=str, required=True)
+    parser.add_argument('--gene_type', type=str, required=True) 
+    args = parser.parse_args()
+
+    with open(args.config) as f:
+        config = json.load(f)
+
+    config['model']['gene_type'] = args.gene_type
+
+
+    seed=2020
+    np.random.seed(seed)
+    tf.random.set_seed(seed)
+    random.seed(seed)
+
+    trainer_ = Trainer(config)
+    trainer_.train()
+
+
+if __name__ == '__main__':
+    main()
